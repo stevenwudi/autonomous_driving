8c08ce47529a1b22f4abd8e3385ee6941de793ff
ACasadevall <arnau.casadevall.saiz@gmail.com>
2017-04-12 02:35:52 +0200

    SegNET, Tiramisu and other fixes implemented ...
    Added manually due to the merge conflicts from the 'dev' branch.

--- code/config/cityscapes_segmentation.py
+++ code/config/cityscapes_segmentation.py
@@ -1,15 +1,15 @@
 # Dataset
 problem_type                 = 'segmentation'  # ['classification' | 'detection' | 'segmentation']
-dataset_name                 = 'camvid'    # Dataset name
+dataset_name                 = 'cityscapes'        # Dataset name
 dataset_name2                = None            # Second dataset name. None if not Domain Adaptation
 perc_mb2                     = None            # Percentage of data from the second dataset in each minibatch
 
 # Model
-model_name                   = 'tiramisu'      # Model to use ['fcn8' | 'lenet' | 'alexNet' | 'vgg16' |  'vgg19' | 'resnet50' | 'InceptionV3']
+model_name                   = 'segnet_basic'  # Model to use ['fcn8' | 'lenet' | 'alexNet' | 'vgg16' |  'vgg19' | 'resnet50' | 'InceptionV3']
 freeze_layers_from           = None            # Freeze layers from 0 to this layer during training (Useful for finetunning) [None | 'base_model' | Layer_id]
 show_model                   = True            # Show the architecture layers
 load_imageNet                = False           # Load Imagenet weights and normalize following imagenet procedure
-load_pretrained              = False           # Load a pretrained model for doing finetuning
+load_pretrained              = False            # Load a pretrained model for doing finetuning
 weights_file                 = 'weights.hdf5'  # Training weight file name
 
 # Parameters
@@ -18,22 +18,22 @@ test_model                   = True            # Test the model
 pred_model                   = False           # Predict using the model
 
 # Debug
-debug                        = False           # Use only few images for debuging
+debug                        = False            # Use only few images for debuging
 debug_images_train           = 50              # N images for training in debug mode (-1 means all)
 debug_images_valid           = 30              # N images for validation in debug mode (-1 means all)
 debug_images_test            = 30              # N images for testing in debug mode (-1 means all)
-debug_n_epochs               = 3               # N of training epochs in debug mode
+debug_n_epochs               = 2               # N of training epochs in debug mode
 
 # Batch sizes
-batch_size_train             = 4              # Batch size during training
-batch_size_valid             = 4              # Batch size during validation
-batch_size_test              = 4              # Batch size during testing
-crop_size_train              = (224, 224)      # Crop size during training (Height, Width) or None
+batch_size_train             = 10              # Batch size during training
+batch_size_valid             = 10              # Batch size during validation
+batch_size_test              = 10              # Batch size during testing
+crop_size_train              = None            # Crop size during training (Height, Width) or None
 crop_size_valid              = None            # Crop size during validation
 crop_size_test               = None            # Crop size during testing
-resize_train                 = (360, 480)      # Resize the image during training (Height, Width) or None
-resize_valid                 = (224, 224)      # Resize the image during validation
-resize_test                  = (224, 224)      # Resize the image during testing
+resize_train                 = (240, 480)      # Resize the image during training (Height, Width) or None
+resize_valid                 = (240, 480)      # Resize the image during validation
+resize_test                  = (240, 480)      # Resize the image during testing
 
 # Data shuffle
 shuffle_train                = True            # Whether to shuffle the training data
@@ -44,15 +44,16 @@ seed_valid                   = 1924            # Random seed for the validation 
 seed_test                    = 1924            # Random seed for the testing shuffle
 
 # Training parameters
-optimizer                    = 'adam'       # Optimizer
-learning_rate                = 0.001          # Training learning rate
-weight_decay                 = 1e-4            # Weight decay or L2 parameter norm penalty
-n_epochs                     = 1000            # Number of epochs during training
+optimizer                    = 'rmsprop'       # Optimizer
+learning_rate                = 0.0001          # Training learning rate
+weight_decay                 = 0.              # Weight decay or L2 parameter norm penalty
+n_epochs                     = 100            # Number of epochs during training
 
 # Callback save results
 save_results_enabled         = False           # Enable the Callback
 save_results_nsamples        = 5               # Number of samples to save
 save_results_batch_size      = 5               # Size of the batch
+
 
 # Callback early stoping
 earlyStopping_enabled        = True            # Enable the Callback
@@ -88,7 +89,7 @@ LRScheduler_S                = 10000            # Step for the 'step' method
 LRScheduler_power            = 0.9              # Power for te poly method
 
 # Callback TensorBoard
-TensorBoard_enabled          = False             # Enable the Callback
+TensorBoard_enabled          = True             # Enable the Callback
 TensorBoard_histogram_freq   = 1                # Frequency (in epochs) at which to compute activation histograms for the layers of the model. If set to 0, histograms won't be computed.
 TensorBoard_write_graph      = True             # Whether to visualize the graph in Tensorboard. The log file can become quite large when write_graph is set to True.
 TensorBoard_write_images     = False            # Whether to write model weights to visualize as image in Tensorboard.
--- code/config/synthia_segmentation.py
+++ code/config/synthia_segmentation.py
@@ -1,20 +1,20 @@
 # Dataset
 problem_type                 = 'segmentation'  # ['classification' | 'detection' | 'segmentation']
-dataset_name                 = 'camvid'    # Dataset name
+dataset_name                 = 'synthia_rand_cityscapes'        # Dataset name
 dataset_name2                = None            # Second dataset name. None if not Domain Adaptation
 perc_mb2                     = None            # Percentage of data from the second dataset in each minibatch
 
 # Model
-model_name                   = 'tiramisu'      # Model to use ['fcn8' | 'lenet' | 'alexNet' | 'vgg16' |  'vgg19' | 'resnet50' | 'InceptionV3']
+model_name                   = 'fcn8'          # Model to use ['fcn8' | 'lenet' | 'alexNet' | 'vgg16' |  'vgg19' | 'resnet50' | 'InceptionV3']
 freeze_layers_from           = None            # Freeze layers from 0 to this layer during training (Useful for finetunning) [None | 'base_model' | Layer_id]
-show_model                   = True            # Show the architecture layers
+show_model                   = False           # Show the architecture layers
 load_imageNet                = False           # Load Imagenet weights and normalize following imagenet procedure
 load_pretrained              = False           # Load a pretrained model for doing finetuning
 weights_file                 = 'weights.hdf5'  # Training weight file name
 
 # Parameters
 train_model                  = True            # Train the model
-test_model                   = True            # Test the model
+test_model                   = True           # Test the model
 pred_model                   = False           # Predict using the model
 
 # Debug
@@ -25,15 +25,15 @@ debug_images_test            = 30              # N images for testing in debug m
 debug_n_epochs               = 3               # N of training epochs in debug mode
 
 # Batch sizes
-batch_size_train             = 4              # Batch size during training
-batch_size_valid             = 4              # Batch size during validation
-batch_size_test              = 4              # Batch size during testing
+batch_size_train             = 10              # Batch size during training
+batch_size_valid             = 10              # Batch size during validation
+batch_size_test              = 10              # Batch size during testing
 crop_size_train              = (224, 224)      # Crop size during training (Height, Width) or None
 crop_size_valid              = None            # Crop size during validation
 crop_size_test               = None            # Crop size during testing
-resize_train                 = (360, 480)      # Resize the image during training (Height, Width) or None
-resize_valid                 = (224, 224)      # Resize the image during validation
-resize_test                  = (224, 224)      # Resize the image during testing
+resize_train                 = (270, 480)      # Resize the image during training (Height, Width) or None
+resize_valid                 = (270, 480)      # Resize the image during validation
+resize_test                  = (270, 480)      # Resize the image during testing
 
 # Data shuffle
 shuffle_train                = True            # Whether to shuffle the training data
@@ -44,15 +44,16 @@ seed_valid                   = 1924            # Random seed for the validation 
 seed_test                    = 1924            # Random seed for the testing shuffle
 
 # Training parameters
-optimizer                    = 'adam'       # Optimizer
-learning_rate                = 0.001          # Training learning rate
-weight_decay                 = 1e-4            # Weight decay or L2 parameter norm penalty
-n_epochs                     = 1000            # Number of epochs during training
+optimizer                    = 'rmsprop'       # Optimizer
+learning_rate                = 0.0001          # Training learning rate
+weight_decay                 = 0.              # Weight decay or L2 parameter norm penalty
+n_epochs                     = 25            # Number of epochs during training
 
 # Callback save results
 save_results_enabled         = False           # Enable the Callback
 save_results_nsamples        = 5               # Number of samples to save
 save_results_batch_size      = 5               # Size of the batch
+
 
 # Callback early stoping
 earlyStopping_enabled        = True            # Enable the Callback
@@ -88,7 +89,7 @@ LRScheduler_S                = 10000            # Step for the 'step' method
 LRScheduler_power            = 0.9              # Power for te poly method
 
 # Callback TensorBoard
-TensorBoard_enabled          = False             # Enable the Callback
+TensorBoard_enabled          = True             # Enable the Callback
 TensorBoard_histogram_freq   = 1                # Frequency (in epochs) at which to compute activation histograms for the layers of the model. If set to 0, histograms won't be computed.
 TensorBoard_write_graph      = True             # Whether to visualize the graph in Tensorboard. The log file can become quite large when write_graph is set to True.
 TensorBoard_write_images     = False            # Whether to write model weights to visualize as image in Tensorboard.
