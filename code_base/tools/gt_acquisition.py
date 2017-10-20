df9ccde42e132b1684ec6757456eadb508e472e9
stevenwudi <stevenwudi@gmail.com>
2017-10-20 12:18:04 +0800

    GT collection and visualisation

--- code_base/tools/gt_acquisition.py
+++ code_base/tools/gt_acquisition.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot as plt
 from matplotlib.patches import Rectangle
+import matplotlib.cm as cm
+import matplotlib
 import numpy as np
 import cv2
 import os
@@ -20,10 +22,11 @@ def convert_key_to_string(car_tracking_dict):
 def convert_list_to_json_serializable(car_tracking_dict):
     # because stupid reason the saved list data type can not be dumped directly to json
     for key in car_tracking_dict.keys():
-        converted_list = []
-        for l in car_tracking_dict[key]['tracking_rect']:
-            converted_list.append([np.int(i) for i in l])
-        car_tracking_dict[str(key)]['tracking_rect'] = converted_list
+        for i, track_list in enumerate(car_tracking_dict[key]['tracking_rect']):
+            converted_list = []
+            for l in track_list:
+                converted_list.append([np.int(i) for i in l])
+            car_tracking_dict[str(key)]['tracking_rect'][i] = converted_list
 
     return car_tracking_dict
 
@@ -113,7 +116,7 @@ def get_ground_truth_box_centre_of_mass(cf, instances, classes):
     plt.show()
 
 
-def get_ground_truth_box(cf, instances, classes, car_tracking_dict, i_frame, img_name, draw_flag=True):
+def get_ground_truth_box(cf, instances, classes, car_tracking_dict, i_frame, img_name, draw_flag=False):
     # now we count the number of pixels for each instances
     unique_labels, counts_label = np.unique(instances, return_counts=True)
     instance_percentage = counts_label / np.prod(cf.resize_train)
@@ -136,46 +139,52 @@ def get_ground_truth_box(cf, instances, classes, car_tracking_dict, i_frame, img
         centreX, centreY = int((top+bottom)/2), int((left+right)/2)
         if classes[centreX, centreY] == cf.classes['car']:
             if draw_flag:
-                if i_frame > 205 and i_frame < 250:
-                    fig, tracking_figure_axes = plt.subplots(1)
-                    tracking_figure_axes.imshow(instances)
-                    tracking_rect = Rectangle(
-                        xy=(centreY - int(width / 2), centreX - int(height / 2)),
-                        width=width,
-                        height=height,
-                        facecolor='none',
-                        edgecolor='r',
-                    )
-                    tracking_figure_axes.add_patch(tracking_rect)
-                    pixel_occupant_rate = instance_percentage[unique_labels == instance_number_in_seq]
-                    tracking_figure_axes.annotate(['{:.4f}'.format(i) for i in pixel_occupant_rate],
-                                                  xy=(centreY, centreX), color='red')
-                    tracking_figure_axes.annotate('car', xy=(centreY, centreX - 10), color='red')
-                    plt.show()
-
+                fig, tracking_figure_axes = plt.subplots(1)
+                tracking_figure_axes.imshow(instances)
+                tracking_rect = Rectangle(
+                    xy=(centreY - int(width / 2), centreX - int(height / 2)),
+                    width=width,
+                    height=height,
+                    facecolor='none',
+                    edgecolor='r',
+                )
+                tracking_figure_axes.add_patch(tracking_rect)
+                pixel_occupant_rate = instance_percentage[unique_labels == instance_number_in_seq]
+                tracking_figure_axes.annotate(['{:.4f}'.format(i) for i in pixel_occupant_rate],
+                                              xy=(centreY, centreX), color='red')
+                tracking_figure_axes.annotate('car', xy=(centreY, centreX - 10), color='red')
+                plt.show()
             # if it is an already tracked car:
             if instance_number_in_seq in car_tracking_dict:
                 # print('continue tracking the car')
-                if int(img_name[:-4]) == (int(car_tracking_dict[instance_number_in_seq]['img_list'][-1][:-4]) + 1):
-                    car_tracking_dict[instance_number_in_seq]['tracking_rect'].append([centreX, centreY, height, width])
-                    car_tracking_dict[instance_number_in_seq]['img_list'].append(img_name)
+                if int(img_name[:-4]) == (int(car_tracking_dict[instance_number_in_seq]['img_list'][-1][-1][:-4]) + 1):
+                    car_tracking_dict[instance_number_in_seq]['tracking_rect'][-1].append([centreX, centreY, height, width])
+                    car_tracking_dict[instance_number_in_seq]['img_list'][-1].append(img_name)
                 else:
-                    print("Tracked car not continuous! Investigate! image name: %s, car instance: %d" % (img_name, instance_number_in_seq))
-                    car_tracking_dict[instance_number_in_seq]['tracking_rect'].append([centreX, centreY, height, width])
-                    car_tracking_dict[instance_number_in_seq]['img_list'].append(img_name)
+                    #print("Tracked car not continuous! Investigate! image name: %s, car instance: %d" % (img_name, instance_number_in_seq))
+                    if instance_number_in_seq in instance_large:
+                        print("Tracked car not continuous! Start another track for this car")
+                        print("image name: %s, car instance: %d" % (img_name, instance_number_in_seq))
+                        car_tracking_dict[instance_number_in_seq]['start_frame'].append(i_frame)
+                        car_tracking_dict[instance_number_in_seq]['img_list'].append([])
+                        car_tracking_dict[instance_number_in_seq]['img_list'][-1].append(img_name)
+                        car_tracking_dict[instance_number_in_seq]['tracking_rect'].append([])
+                        car_tracking_dict[instance_number_in_seq]['tracking_rect'][-1].append([centreX, centreY, height, width])
+
             # else we initiate a instance_number_in_seq as car ID to start tracking
             else:
                 if instance_number_in_seq in instance_large:
                     print('start tracking a new car %d' % instance_number_in_seq)
                     car_tracking_dict[instance_number_in_seq] = {}
                     car_tracking_dict[instance_number_in_seq]['dir'] = os.path.join(cf.dataset_path, cf.data_type, cf.data_stereo, cf.data_camera)
-                    car_tracking_dict[instance_number_in_seq]['start_frame'] = i_frame
-                    car_tracking_dict[instance_number_in_seq]['img_list'] = []
-                    car_tracking_dict[instance_number_in_seq]['img_list'].append(img_name)
-                    car_tracking_dict[instance_number_in_seq]['tracking_rect'] = []
-                    car_tracking_dict[instance_number_in_seq]['tracking_rect'].append([centreX, centreY, height, width])
+                    car_tracking_dict[instance_number_in_seq]['start_frame'] = []
+                    car_tracking_dict[instance_number_in_seq]['start_frame'].append(i_frame)
+                    car_tracking_dict[instance_number_in_seq]['img_list'] = [[]]
+                    car_tracking_dict[instance_number_in_seq]['img_list'][0].append(img_name)
+                    car_tracking_dict[instance_number_in_seq]['tracking_rect'] = [[]]
+                    car_tracking_dict[instance_number_in_seq]['tracking_rect'][0].append([centreX, centreY, height, width])
 
-        return car_tracking_dict
+    return car_tracking_dict
 
 
 def get_ground_truth_sequence_car_trajectory(DG, cf, show_set='train'):
@@ -212,12 +221,69 @@ def get_ground_truth_sequence_car_trajectory(DG, cf, show_set='train'):
     json_file_path = os.path.join(cf.savepath, cf.sequence_name+'.json')
 
     # Di Wu's comment: json key must be string, duh....
-    car_tracking_dict_copy = car_tracking_dict.copy()
-    car_tracking_dict = convert_list_to_json_serializable(convert_key_to_string(car_tracking_dict))
+    car_tracking_dict = convert_key_to_string(car_tracking_dict)
+    car_tracking_dict = convert_list_to_json_serializable(car_tracking_dict)
     with open(json_file_path, 'w') as fp:
         json.dump(car_tracking_dict, fp, indent=4)
 
     return car_tracking_dict
 
 
+def draw_selected_gt_car_trajectory(DG, cf, show_set='train'):
+    """
+    This script is used to visualise the collected GT
+    :param DG:
+    :param cf:
+    :return:
+    """
+    json_file_path = os.path.join(cf.savepath, cf.sequence_name + '.json')
+    with open(json_file_path) as json_data:
+        car_tracking_dict = json.load(json_data)
+
+    print("car instance ids are: " + ', '.join(['%s' % key for key in car_tracking_dict.keys()]))
+    print('Total number of cars is %d.' % len(car_tracking_dict.keys()))
+    cm_range = np.array([int(x) for x in car_tracking_dict.keys()])
+    norm = matplotlib.colors.Normalize(vmin=cm_range.min(), vmax=cm_range.max())
+    cmap = cm.jet
+    m = cm.ScalarMappable(norm=norm, cmap=cmap)
+    fig, tracking_figure_axes = plt.subplots()
+
+    train_set = DG.dataloader[show_set]
+    for i_batch, sample_batched in enumerate(train_set):
+        instances_torch = sample_batched['instances'][0]
+        classes_torch = sample_batched['classes'][0]
+        instances = instances_torch.numpy().astype('uint8')
+        classes = classes_torch.numpy().astype('uint8')
+        img_name = sample_batched['img_name'][0]  # because for GT construction, the batch size is always 1
+
+        draw_ground_truth_box(tracking_figure_axes, m, cf, instances, classes, car_tracking_dict, i_batch, img_name)
+
+
+def draw_ground_truth_box(tracking_figure_axes, m, cf, instances, classes, car_tracking_dict, i_batch, img_name):
+    tracking_figure_axes.clear()
+    tracking_figure_axes.imshow(instances)
+    unique_labels, counts_label = np.unique(instances, return_counts=True)
+    instance_percentage = counts_label / np.prod(cf.resize_train)
+
+    for instance_number_in_seq in car_tracking_dict.keys():
+        car_color = m.to_rgba(int(instance_number_in_seq))
+        for track_num, img_lists_all in enumerate(car_tracking_dict[instance_number_in_seq]['img_list']):
+            if img_name in img_lists_all:
+                im_num = img_lists_all.index(img_name)
+                [centreX, centreY, height, width] = car_tracking_dict[instance_number_in_seq]['tracking_rect'][track_num][im_num]
+                tracking_rect = Rectangle(
+                    xy=(centreY - int(width / 2), centreX - int(height / 2)),
+                    width=width,
+                    height=height,
+                    facecolor='none',
+                    edgecolor=car_color,
+                )
+                tracking_figure_axes.add_patch(tracking_rect)
+                pixel_occupant_rate = instance_percentage[unique_labels == int(instance_number_in_seq)]
+                tracking_figure_axes.annotate('{:.4f}'.format(pixel_occupant_rate[0]),
+                                              xy=(centreY - int(width / 2), centreX), color=car_color)
+                car_id = instance_number_in_seq + '_' + str(0)
+                tracking_figure_axes.annotate(car_id, xy=(centreY - int(width / 2), centreX - int(height / 2) - 5), color=car_color)
+    plt.title(img_name)
+    plt.waitforbuttonpress(0.01)
 
