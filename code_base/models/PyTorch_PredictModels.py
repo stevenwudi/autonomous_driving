e9bd5e3115b8765586f9d92f50f38ae2c54807f4
wzn0828 <18001279185@163.com>
2017-11-09 13:15:34 +0800

    modify lstmToFc modle for multilayer lstm

--- code_base/models/PyTorch_PredictModels.py
+++ code_base/models/PyTorch_PredictModels.py
@@ -18,7 +18,7 @@ class LSTM_ManyToMany(nn.Module):
         :param hidden_sizes: a list involves each lstm_layer's hidden_size
         :param outlayer_input_dim:
         :param outlayer_output_dim:
-        :param cuda: whetuer or not to use cuda
+        :param cuda: whether or not to use cuda
         '''
         super(LSTM_ManyToMany, self).__init__()
         # some superParameters & input output dimensions
@@ -68,7 +68,7 @@ class LSTM_ManyToMany(nn.Module):
         # result of train or test, shape(batch size,sequence size, feature size)
         outputs += [outputs_linear]
 
-        # result of predict, shape as (batch size,feture, feature size)
+        # result of predict, shape as (batch size,future, feature size)
         outputs_pre = [outputs_linear[:, -1, :]]
         # if we should predict the future
         if future-1 > 0:
@@ -97,36 +97,41 @@ class LSTM_To_FC(nn.Module):
     This model use an LSTM to fuse features of all past frames into a single state, which inputs to a following Fully Connected model to predict next some frames' features.
     The lstm model can be seen as a encoder, while the FC model as a decoder.
     """
-    def __init__(self, future, input_dim, hidden_size, num_layers, output_dim, cuda=True):
-        """
-        future: The number of predicting frames
-        input_dim: The number of features in each frame
-        hidden_size: The number of features in the hidden state of lstm
-        num_layers: The number of recurrent layers in lstm
-        self.output_size: The number of fc's outputs
-        self.linear1_output_size: The number of cells hidden layer of fc model
-        """
+    def __init__(self,  input_dims, hidden_sizes, future_frame, output_dim, cuda=True):
+        '''
+
+        :param input_dims: a list involves each lstm_layer's input_dim
+        :param hidden_sizes: a list involves each lstm_layer's hidden_size
+        :param future_frame: the number of predicting frames
+        :param output_dim: the number of features in each predicting frame
+        :param cuda: whether or not to use GPU
+        '''
         super(LSTM_To_FC, self).__init__()
-        self.future = future
-        self.input_dim = input_dim
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
+        # lstm part
+        self.input_dims = input_dims
+        self.hidden_sizes = hidden_sizes
+        # fully connected part
+        self.future_frame = future_frame
         self.output_dim = output_dim
-        self.output_size = self.future * self.output_dim
-        self.linear1_output_size = int((self.hidden_size + self.output_size)/2)
+        self.output_size = self.future_frame * self.output_dim
+        self.fc_hidden1_size = int((self.hidden_sizes[-1] + self.output_size)/2)
 
         # build lstm layer, parameters are (input_dim,hidden_size,num_layers)
-        self.lstm1 = nn.LSTM(self.input_dim, self.hidden_size, self.num_layers, batch_first=True)
+        for i, input_dim in enumerate(self.input_dims):
+            self.__setattr__('lstm' + str(i),
+                             nn.LSTM(input_size=input_dim, hidden_size=self.hidden_sizes[i], num_layers=1,
+                                     batch_first=True))
+
         # build fc model
-        self.linear1 = nn.Linear(self.hidden_size, self.linear1_output_size)
+        self.linear1 = nn.Linear(self.hidden_sizes[-1], self.fc_hidden1_size)
         self.nonlinear1 = nn.ReLU()
-        self.linear2 = nn.Linear(self.linear1_output_size, self.output_size)
+        self.linear2 = nn.Linear(self.fc_hidden1_size, self.output_size)
         if cuda:
             self.dtype = torch.cuda.FloatTensor
         else:
             self.dtype = torch.FloatTensor
 
-    def forward(self, input, future=0):
+    def forward(self, input):
         """
         input: The train data or test data, type is Variable, size is (batchSize, sequenceSize,featureSize)
         future: The number of predicting frames, but this parameter is invalid, it is determined by _init_
@@ -134,22 +139,28 @@ class LSTM_To_FC(nn.Module):
         """
         # the return content
         outputs = []
-        # init hidden state & cell state,  parameters are (numlayers, batchsize, hidden_size)
-        h_0 = Variable(torch.zeros(self.num_layers, input.size(0), self.hidden_size).type(self.dtype), requires_grad=False)
-        c_0 = Variable(torch.zeros(self.num_layers, input.size(0), self.hidden_size).type(self.dtype), requires_grad=False)
 
         # compute with train or test data,the output is hidden_state & cell_state
-        # the hn1[-1] represents hidden_state in last lstm layer, and it involves all the past information through the recurrent process
-        outputs_lstm1, (hn1, cn1) = self.lstm1(input, (h_0, c_0))
+        # the hn represents hidden_state in each lstm layer, and it involves all the past information through the recurrent process
+        # compute
+        batch_size = input.size(0)
+        input_t = input
+        for i in range(0, len(self.hidden_sizes)):
+            # init hidden state & cell state, parameters are (numlayers, batchsize, hidden_size)
+            h0 = Variable(torch.zeros(1, batch_size, self.hidden_sizes[i]).type(self.dtype), requires_grad=False)
+            c0 = Variable(torch.zeros(1, batch_size, self.hidden_sizes[i]).type(self.dtype), requires_grad=False)
+            lstm = self.__getattr__('lstm'+str(i))
+            output_t, (hn, cn) = lstm(input_t, (h0, c0))
+            input_t = output_t
 
         # compute the forward in FC model
-        output_linear1 = self.linear1(hn1[-1])
+        output_linear1 = self.linear1(hn[-1, :, :])
         output_nonlinear1 = self.nonlinear1(output_linear1)
         output_linear2 = self.linear2(output_nonlinear1)
 
         # resize to (batchsize, frame_num, featureNumInEachFrame)
         size = output_linear2.size()
-        output_linear2 = output_linear2.resize(size[0], self.future, self.input_dim)
+        output_linear2 = output_linear2.resize(size[0], self.future_frame, self.output_dim)
         outputs.append(output_linear2)
 
         return outputs
