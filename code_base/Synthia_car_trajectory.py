181b200b83557842161b18c546614fe388684efa
wzn0828 <18001279185@163.com>
2017-11-10 12:27:53 +0800

    for local

--- code_base/Synthia_car_trajectory.py
+++ code_base/Synthia_car_trajectory.py
@@ -1,7 +1,8 @@
 import argparse
 import os
 import sys
-os.environ('CUDA_VISIBLE_DEVICES')='1'
+import os
+os.environ['CUDA_VISIBLE_DEVICES']='3'
 # Di Wu add the following really ugly code so that python can find the path
 sys.path.append(os.getcwd())
 import time
